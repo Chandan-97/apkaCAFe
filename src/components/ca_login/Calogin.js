17d369a829e037d2fab91479dbc976492929ee5f
thefullstackboy <66158784+thefullstackboy@users.noreply.github.com>
2023-04-24 17:04:43 +0530

    ca login done

--- src/components/ca_login/Calogin.js
+++ src/components/ca_login/Calogin.js
@@ -1,23 +1,47 @@
-import React from 'react'
 import {Link } from "react-router-dom";
-import '../ca_login/login.css'
+import '../customer_login/customer.css'
 import Header from '../header/header';
-import Banner from '../banner/banner';
+import axios from 'axios';
+import { useForm } from "react-hook-form";
+// import AuthUser from '../authUser/AuthUser';
+import { useNavigate } from "react-router";
 
+export default function Calogin() { 
+  // const {http,setToken} = AuthUser();
 
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const history = useNavigate();
 
-export default function calogin() {
+  const onSubmit = (data)=>{
+    try { 
+     
+      axios.post('http://13.234.30.172:8000/accounts/ca/login',
+        {      
+          email:data.Email,
+          password:data.password
+        }).then((status)=>{       
+          alert("login successfully")     
+          console.log(status.status)
+          history("/");
+        })      
+      } 
+      
+   
+     catch (e) {
+      console.log(e)
+      alert("Your account not created.")
+    }
+  } 
   return (
-    <div> 
-      <Header/>
-      <Banner/>
+    <div>
+          <Header/>           
 <div className="container">
     <div className="row">
       <div className="col-md-6 offset-md-3">
       
         <div className="card my-5">
 
-          <form className="card-body cardbody-color p-lg-2">
+          <form className="card-body cardbody-color p-lg-2" onSubmit={handleSubmit(onSubmit)}>
           <h2 className="text-center mt-5 calogin" id="calogin">CA Login Panel</h2>
 
             <div className="text-center">
@@ -26,11 +50,19 @@ export default function calogin() {
             </div>
 
             <div className="mb-3">
-              <input type="text" className="form-control" id="Username" aria-describedby="emailHelp"
-               placeholder="email/mobile"/>
+              <input  className="form-control" id="Username" aria-describedby="emailHelp"
+                placeholder="email/mobile" 
+                {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} 
+                />
+                  {errors.Email && <p className='text-danger12'>* Please check the email id.</p>}
+
             </div>
             <div className="mb-3">
-              <input type="password" className="form-control" id="password" placeholder="password"/>
+              <input type="password" className="form-control" id="password" placeholder="password"
+             {...register("password",
+             {required: true})}  
+              />
+               {errors.password && <p className='text-danger12'>* password required</p>}               
             </div>
             <div className="text-center"><button type="submit" className="btn btn-color px-5 mb-5 w-100">Login</button></div>
             <div  className="form-text text-center mb-5 emailHelp">Not
