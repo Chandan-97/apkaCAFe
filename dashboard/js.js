5d6f93c17afd6aa956b9903fe340a7190738657e
thefullstackboy <66158784+thefullstackboy@users.noreply.github.com>
2023-04-28 12:42:49 +0530

    ca list

---
+++ dashboard/js.js
@@ -0,0 +1,50 @@
+document.getElementById('star-1').onclick = function() {
+    var radios = document.getElementsByName('contact');
+    for (var radio of radios)
+    {
+        if (radio.checked) {
+            console.log(radio.value);
+        }
+    }
+}
+
+document.getElementById('star-2').onclick = function() {
+    var radios = document.getElementsByName('contact');
+    for (var radio of radios)
+    {
+        if (radio.checked) {
+            console.log(radio.value);
+        }
+    }
+}
+
+document.getElementById('star-3').onclick = function() {
+    var radios = document.getElementsByName('contact');
+    for (var radio of radios)
+    {
+        if (radio.checked) {
+            console.log(radio.value);
+        }
+    }
+}
+
+document.getElementById('star-4').onclick = function() {
+    var radios = document.getElementsByName('contact');
+    for (var radio of radios)
+    {
+        if (radio.checked) {
+            console.log(radio.value);
+        }
+    }
+}
+
+document.getElementById('star-5').onclick = function() {
+    var radios = document.getElementsByName('contact');
+    for (var radio of radios)
+    {
+        if (radio.checked) {
+            console.log(radio.value);
+        }
+    }
+}
+
